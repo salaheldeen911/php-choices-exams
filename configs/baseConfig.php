394a5119d9b063ff490a82da241f07c98ba64ae0
salaheldeen911 <68305647+salaheldeen911@users.noreply.github.com>
2022-05-06 04:11:05 +0200

    Update baseConfig.php

--- configs/baseConfig.php
+++ configs/baseConfig.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
-// define('ROOT_SERVER', $_SERVER['DOCUMENT_ROOT'] . 'php-for-job2/');
-// define('ROOT_URL', '/php-for-job2');
 
-$_SESSION['ROOT_SERVER'] = $_SERVER['DOCUMENT_ROOT'] . 'php-for-job2/';
-$_SESSION['ROOT_URL'] = '/php-for-job2/';
+$_SESSION['ROOT_SERVER'] = $_SERVER['DOCUMENT_ROOT'] . 'php_choices_exams/';
+$_SESSION['ROOT_URL'] = '/php_choices_exams/';
 
 
 
