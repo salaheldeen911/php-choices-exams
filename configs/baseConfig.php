458dd2a840f2ec6b0e3f32bf9c643f7bb9f4f382
salah <salah.eldeen.mail@gmail.com>
2022-09-19 20:24:40 +0200

    update configs

--- configs/baseConfig.php
+++ configs/baseConfig.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
 
-$_SESSION['ROOT_SERVER'] = $_SERVER['DOCUMENT_ROOT'] . 'php_choices_exams/';
-$_SESSION['ROOT_URL'] = '/php_choices_exams/';
+$array = explode('\\', __dir__);
+array_pop($array);
+$root = $array[count($array) - 1];
+$str = str_replace("\\", "/", __dir__);
+$str2 = str_replace($_SERVER['DOCUMENT_ROOT'], "", $str);
 
+$root_url = str_replace("/configs", "", $str2);
+$root_server = str_replace("$root\configs", "", __dir__) . $root . "\\";
 
-
-
-
-
+$_SESSION['ROOT_SERVER'] = $root_server;
+$_SESSION['ROOT_URL'] = "/" . $root_url . "/";
